09eaf7c3204c2c2fa8e92a271cba158689326313
garfieldduck <garfieldduck@gmail.com>
2020-05-10 23:31:12 +0200

    Add content for eslint

--- packages/config-eslint/__tests__/config-eslint.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const configEslint = require('..');
-
-describe('@garfieldduck/config-eslint', () => {
-    it('needs tests');
-});
---
+++ packages/config-eslint/index.js
@@ -0,0 +1,75 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "airbnb",
+    "plugin:react/recommended",
+    "plugin:import/typescript",
+    // recommended rules from the @typescript-eslint/eslint-plugin
+    "plugin:@typescript-eslint/recommended",
+    "plugin:mdx/recommended",
+    // uses eslint-config-prettier to disable ESLint rules
+    "prettier/@typescript-eslint",
+    // displays prettier errors as ESLint errors
+    "plugin:prettier/recommended"
+  ],
+  plugins: ["@typescript-eslint", "react-hooks"],
+  parserOptions: {
+    // allows for the parsing of modern ECMAScript features
+    ecmaVersion: 2018,
+    // allows for the use of imports
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  rules: {
+    // ======================================================
+    // Import rules
+    // ======================================================
+    // Ref: https://basarat.gitbooks.io/typescript/docs/tips/defaultIsBad.html
+    "import/prefer-default-export": "off",
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        "js": "never",
+        "jsx": "never",
+        "ts": "never",
+        "tsx": "never"
+      }
+   ],
+
+    // ======================================================
+    // React rules
+    // ======================================================
+    "react/jsx-filename-extension": [
+      "warn",
+      {
+        extensions: [".jsx", ".tsx"]
+      }
+    ],
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "react/jsx-props-no-spreading": [
+      "error",
+      {
+        custom: "ignore"
+      }
+    ],
+    "react/prop-types": "off",
+    "react/display-name": "off",
+
+    // ======================================================
+    // Typescript rules
+    // ======================================================
+    "@typescript-eslint/explicit-function-return-type": "off"
+  },
+  settings: {
+    react: {
+      version: "detect"
+    }
+  },
+  env: {
+    browser: true
+  }
+};
