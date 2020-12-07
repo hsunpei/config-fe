332dbd05e973f587c4abd9a73e34b94a42a4ccea
HsunPei Wang <garfieldduck@gmail.com>
2020-12-07 17:48:02 +0800

    Refine stylelint's settings

--- packages/stylelint-config/index.js
+++ packages/stylelint-config/index.js
@@ -2,6 +2,12 @@ module.exports = {
   processors: ["stylelint-processor-styled-components"],
   extends: [
     "stylelint-config-recommended",
+    "stylelint-prettier/recommended",
     "stylelint-config-styled-components",
+    "stylelint-config-idiomatic-order",
   ],
+  rules: {
+    "block-closing-brace-newline-after": "always",
+    "prettier/prettier": true,
+  },
 };
