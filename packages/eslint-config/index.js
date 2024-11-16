1488e29c770e30bc740c558751b011ae3db35e80
Hsun-Pei Wang <garfieldduck@gmail.com>
2024-11-16 19:37:07 +0100

    🔧 : remove consistent-type-imports

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -96,8 +96,6 @@ module.exports = {
 
     "no-shadow": "off",
     "@typescript-eslint/no-shadow": ["error"],
-    "@typescript-eslint/consistent-type-exports": "error",
-    "@typescript-eslint/consistent-type-imports": "error",
 
     // ======================================================
     // A11y rules
